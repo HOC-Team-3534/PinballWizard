f3be892124c66d8e7a870571c457bdc2fda35a4b
3534programming <3534programming@gmail.com>
2020-02-12 19:50:42 -0500

    Adde elevate function

---
+++ src/main/java/org/usfirst/frc3534/RobotBasic/functions/Elevate.java
@@ -0,0 +1,84 @@
+package org.usfirst.frc3534.RobotBasic.functions;
+
+import org.usfirst.frc3534.RobotBasic.Robot;
+import org.usfirst.frc3534.RobotBasic.RobotMap;
+import org.usfirst.frc3534.RobotBasic.OI.Axes;
+import org.usfirst.frc3534.RobotBasic.OI.Buttons;
+import org.usfirst.frc3534.RobotBasic.RobotMap.FunctionStateDelay;
+import org.usfirst.frc3534.RobotBasic.systems.Elevator.ElevatorState;
+import org.usfirst.frc3534.RobotBasic.systems.Intake.IntakeArmState;
+import org.usfirst.frc3534.RobotBasic.systems.Intake.IntakeRollerState;
+import org.usfirst.frc3534.RobotBasic.systems.Shooter.IndexWheelState;
+import org.usfirst.frc3534.RobotBasic.systems.Shooter.ShooterState;
+import org.usfirst.frc3534.RobotBasic.systems.Shooter.TopBeltState;
+
+public class Elevate extends FunctionBase implements FunctionInterface{
+
+    long originalTime = 0;
+
+    public Elevate(){
+
+        reset();
+        completed();
+
+    }
+
+    @Override
+    public void process(){
+
+        if(!running && Math.abs(Axes.Elevate_UpAndDown.getAxis()) >= 0.25){
+
+            this.reset();
+
+        }
+        
+        if(this.state == State.ready.s){
+
+            if(Math.abs(Axes.Elevate_UpAndDown.getAxis()) >= 0.25){
+
+                this.started();
+                this.state = State.elevate.s;
+                
+            }
+
+        }
+
+        if(this.state == State.elevate.s){
+
+            Robot.elevator.setElevatorState(ElevatorState.up_down);
+            this.state = State.dead.s;
+
+        }
+
+        if(this.state == State.dead.s){
+
+        }
+
+        if(Math.abs(Axes.Elevate_UpAndDown.getAxis()) < 0.25){
+
+            this.state = State.end.s;
+
+        }
+
+        if(this.state == State.end.s){
+
+            completed();
+
+        }
+
+    }
+
+    private enum State{
+        dead(-1),
+        ready(0),
+        elevate(10),
+        end(100);
+
+        int s;
+
+        private State(int s){
+            this.s = s;
+        }
+    }
+
+}
