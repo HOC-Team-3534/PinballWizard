ee190b0dce21509be3c200401a95fb80bab152b3
Malachi Bergman <34252403+mjbergman92@users.noreply.github.com>
2020-02-21 14:31:38 -0500

    adding FunctionProcessor system.out.prints

--- src/main/java/org/usfirst/frc3534/RobotBasic/functions/FunctionProcessor.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/functions/FunctionProcessor.java
@@ -47,46 +47,55 @@ public class FunctionProcessor{
 
         if(!shootClose.isRunning()){
 
+            System.out.println("Shoot Far Processing...");
             shootFar.process();
 
         }
         if(!shootFar.isRunning() && !Robot.elevator.isWinchClimbing() && false){
 
+            System.out.println("Shoot Close Processing...");
             shootClose.process();
 
         }
         if(!Robot.elevator.isWinchClimbing()){
 
+            System.out.println("Intake Processing...");
             intake.process();
 
         }
         if(!elevate.isRunning() && !climb.isRunning() && !positionControl.isRunning() && !Robot.elevator.isWinchClimbing() && false){
 
+            System.out.println("Rotation Control Processing...");
             rotationControl.process();
 
         }
         if(!elevate.isRunning() && !climb.isRunning() && !rotationControl.isRunning() && !Robot.elevator.isWinchClimbing() && false){
 
+            System.out.println("Position Control Processing...");
             positionControl.process();
 
         }
         if(!climb.isRunning()){
 
+            System.out.println("Translate Processing...");
             translate.process();
 
         }
         if(!elevate.isRunning() && !positionControl.isRunning() && !rotationControl.isRunning() && !translate.isRunning()){
 
+            System.out.println("Climb Processing...");
             climb.process();
 
         }
         if(!climb.isRunning() && !positionControl.isRunning() && !rotationControl.isRunning() && !translate.isRunning() && !Robot.elevator.isWinchClimbing()){
 
+            System.out.println("Elevate Processing...");
             elevate.process();
 
         }
         if(!shootFar.isRunning() && !shootClose.isRunning() && false){
 
+            System.out.println("AutoIndex Processing...");
             autoIndex.process();
 
         }
